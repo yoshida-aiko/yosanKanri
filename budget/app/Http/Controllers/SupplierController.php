10467afe6f5a8a2971e6aaf4c583a5107f41c42b
yoshida.aiko <yoshida.aiko@infogram.co.jp>
2020-09-08 16:27:11 +0900

    発注マスタ　新規作成

--- budget/app/Http/Controllers/SupplierController.php
+++ budget/app/Http/Controllers/SupplierController.php
@@ -15,8 +15,10 @@ class SupplierController extends Controller
      */
     public function index()
     {
-        //
-        return view('Supplier/index');
+        $Suppliers = Supplier::where('Status','=', 1)->get();
+        $editSupplier = new Supplier();
+
+        return view('Supplier/index',compact('Suppliers','editSupplier'));
     }
 
     /**
@@ -26,7 +28,9 @@ class SupplierController extends Controller
      */
     public function create()
     {
-        //
+        $Supplier = new Supplier();
+
+        return view('Supplier\create',compact('Supplier'));
     }
 
     /**
@@ -37,7 +41,43 @@ class SupplierController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        if($request->action === 'back') {
+            return redirect()->route('Supplier.index');
+        }
+
+        $isUpdate = false;
+
+        if ($request->id != "")
+        {
+            $isUpdate = true;
+        }
+
+        $rules = [
+            'SupplierNameJp' => ['required', 'string', 'max:50'],
+            'ChargeUserJp' => ['nullable', 'string', 'min:50'],
+            'Tel' => ['nullable','string', 'max:20'],
+            'Fax' => ['nullable','string', 'max:20'],
+            'EMail' => ['required', 'string', 'email', 'max:100', Rule::unique('suppliers', 'EMail')->whereNull('deleted_at')->whereNot('EMail', $request->EMail)]
+        ];
+        $this->validate($request, $rules);
+
+        if ($isUpdate){
+            $Supplier = Supplier::findOrFail($request->id);
+        }
+        else {
+            $Supplier = new Supplier();
+        }
+        $Supplier->SupplierNameJp = $request->SupplierNameJp;
+        $Supplier->ChargeUserJp = $request->ChargeUserJp;
+        $Supplier->Tel = $request->Tel;
+        $Supplier->Fax = $request->Fax;
+        $Supplier->EMail = $request->EMail;
+
+        $Supplier->save();
+        $Suppliers = Supplier::where('Status','=', 1)->get();
+        $editSupplier = new Supplier();
+
+        return view('Supplier/index',compact('Suppliers'));
     }
 
     /**
@@ -59,7 +99,9 @@ class SupplierController extends Controller
      */
     public function edit($id)
     {
-        //
+        $Suppliers = Supplier::where('Status','=', 1)->get();
+        $editSupplier = Supplier::findOrFail($id);
+        return view('Supplier/index',compact('Suppliers','editSupplier'));
     }
 
     /**
@@ -71,7 +113,26 @@ class SupplierController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $rules = [
+            'SupplierNameJp' => ['required', 'string', 'max:50'],
+            'ChargeUserJp' => ['string', 'min:50'],
+            'Tel' => ['string', 'max:20'],
+            'Fax' => ['string', 'max:20'],
+            'EMail' => ['required', 'string', 'email', 'max:100']
+        ];
+        $this->validate($request, $rules);
+
+        $Supplier = Supplier::findOrFail($id);
+        $Supplier->SupplierNameJp = $request->SupplierNameJp;
+        $Supplier->ChargeUserJp = $request->ChargeUserJp;
+        $Supplier->Tel = $request->Tel;
+        $Supplier->Fax = $request->Fax;
+        $Supplier->EMail = $request->EMail;
+        $Supplier->save();
+ 
+        $editSupplier = new Supplier();
+
+        return view('Supplier/index',compact('Suppliers','editSupplier'));
     }
 
     /**
@@ -82,6 +143,9 @@ class SupplierController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $Supplier = Supplier::findOrFail($id);
+        $Supplier->delete();
+
+        return redirect()->route('Supplier.index');
     }
 }
--- budget/resources/views/Supplier/index.blade.php
+++ budget/resources/views/Supplier/index.blade.php
@@ -13,7 +13,21 @@
                 <th style="min-width:120px; ">担当者</th>
             </thead>
             <tbody>
-            
+            <tbody>
+            @foreach($Suppliers as $Supplier)
+            <tr>
+                <td>
+                    <form id="frmUserDelete" action="{{ route('Supplier.destroy', $Supplier->id) }}" method='post'>
+                        @csrf
+                        @method('DELETE')
+                        <input type="submit" value="&#xf1f8;" 
+                            onClick="if (!confirm('削除しますか？')){ return false;} return true;" class="fa btn-delete-icon">
+                    </form>
+                </td>
+                <td style="min-width:120px; width:280px"><a href="{{ route('Supplier.edit', $Supplier->id) }}">{{$Supplier->SupplierNameJp}}</a></td>
+                <td style="min-width:120px;"><a href="{{ route('Supplier.edit', $Supplier->id) }}">{{$Supplier->ChargeUserJp}}</a></td>
+            </tr>
+            @endforeach
             </tbody>
 
             </table>
