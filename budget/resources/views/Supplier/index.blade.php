288d0b4eff0eff123bc4a6941ae298be50f58dc6
yoshida.aiko <yoshida.aiko@infogram.co.jp>
2020-09-09 14:09:04 +0900

    発注先マスタ　右側の一覧修正

--- budget/resources/views/Supplier/index.blade.php
+++ budget/resources/views/Supplier/index.blade.php
@@ -6,34 +6,33 @@
 <h5 class="master-title">発注先マスタ</h5>
 <div class="wrapper">
     <div class="divMasterList">
-        <table id="tblSupplierMasterList" class="table table-fixed table-masterFixed table-striped">
+        <table id="tblSupplierMasterList" class="table table-fixed table-masterFixed masterSupplier table-striped">
             <thead>
                 <th></th>
-                <th style="min-width:120px; width:280px">発注先</th>
-                <th style="min-width:120px; ">担当者</th>
+                <th>発注先</th>
+                <th>担当者</th>
             </thead>
-            <tbody>
             <tbody>
             @foreach($Suppliers as $Supplier)
             <tr>
                 <td>
-                    <form id="frmUserDelete" action="{{ route('Supplier.destroy', $Supplier->id) }}" method='post'>
+                    <form id="frmSupplierDelete" action="{{ route('Supplier.destroy', $Supplier->id) }}" method='post'>
                         @csrf
                         @method('DELETE')
                         <input type="submit" value="&#xf1f8;" 
                             onClick="if (!confirm('削除しますか？')){ return false;} return true;" class="fa btn-delete-icon">
                     </form>
                 </td>
-                <td style="min-width:120px; width:280px"><a href="{{ route('Supplier.edit', $Supplier->id) }}">{{$Supplier->SupplierNameJp}}</a></td>
-                <td style="min-width:120px;"><a href="{{ route('Supplier.edit', $Supplier->id) }}">{{$Supplier->ChargeUserJp}}</a></td>
+                <td><a href="{{ route('Supplier.edit', $Supplier->id) }}">{{$Supplier->SupplierNameJp}}</a></td>
+                <td>{{$Supplier->ChargeUserJp}}</td>
             </tr>
             @endforeach
             </tbody>
 
-            </table>
+        </table>
     </div>
     <div class="divMasterInput">
-
+       
     </div>
 </div>
 </div>
