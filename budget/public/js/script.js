d4e9b0c7bdabcdb87b9244ece39c0f58038a193a
yoshida.aiko <yoshida.aiko@infogram.co.jp>
2020-09-09 14:05:21 +0900

    jsファイル更新

--- budget/public/js/script.js
+++ budget/public/js/script.js
@@ -1,6 +1,41 @@
 jQuery (function ()
 {
+    //画像やカレンダーからドロップできてしまっていたので追加 2017/12/27
+    $(document).on('drop dragover', function (e) {
+        e.stopPropagation();
+        e.preventDefault();
+    });
 
+    $(document).keydown(function (event) {
+        // クリックされたキーのコード
+        var keyCode = event.keyCode;
+        // Ctrlキーがクリックされたか (true or false)
+        var ctrlClick = event.ctrlKey;
+        // Altキーがクリックされたか (true or false)
+        var altClick = event.altKey;
+        // キーイベントが発生した対象のオブジェクト
+        var obj = event.target;
+
+        // バックスペースキーを制御する
+        if (keyCode == 8) {
+            // テキストボックス／テキストエリアを制御する
+            if ((obj.tagName.toUpperCase() == "INPUT" && obj.type.toUpperCase() == "TEXT")
+                || obj.tagName.toUpperCase() == "TEXTAREA") {
+                // 入力できる場合は制御しない
+                if (!obj.readOnly && !obj.disabled) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        // Alt + ←を制御する
+        if (altClick && (keyCode == 37 || keyCode == 39)) {
+            return false;
+        }
+
+    });
+    
     $(".table-fixed tr").click(function() {
         $(".table-fixed tr").removeClass("table-fixed-selectRow");
         $(".table-fixed tr").removeClass("table-fixed-nonselect");
--- budget/public/js/searchPageScript.js
+++ budget/public/js/searchPageScript.js
@@ -1,21 +1,205 @@
-jQuery (function ()
+jQuery (function ($)
 {  
-    /*items = document.getElementById('table-searchFixed').getElementsByClassName('table-searchFixed');
+    /*$("#table-searchFixed-tr").draggable();
 
-    Array.prototype.forEach.call(items, function (item) {
-        $(item).on('dragstart', onDragStart);
-        $(item).on('dragend', onDragEnd);
+    $("#sectionOrderRequest").droppable({
+        accept : ".table-searchFixed-tr",
+        drop: function(event, ui) {
+            console.log("drop" + ui);
+        }
+    });*/
+
+
+    $(".numCartOrderRequestNumber").on('input', function() {
+        var cartid = $(this).parent("div").children("input[name=CartId]").val();
+        var ordernumber = $(this).val();
+        console.log("cartid:" + cartid + " ordernumber:" + ordernumber);
+        $.ajax({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            url: 'SearchPage/' + cartid + '',
+            type: 'GET',
+            datatype: 'json',
+            data : {'update_id' : cartid, 'parent_id' : -1, 'delete_id' : -1, 'order_number' : ordernumber}
+        })
+        // Ajaxリクエスト成功時の処理
+        .done(function(data) {
+            if (data['status'] !== 'OK') {
+                alert('データ更新に失敗しました');
+            }
+        })
+        // Ajaxリクエスト失敗時の処理
+        .fail(function(data) {
+            alert('データ更新に失敗しました');
+        });
     });
 
-    // dropzoneのリスナーを設定
-    var $dropzone = $('#sectionOrderRequest')
-        .on('dragover', onDragOver)
-        .on('dragenter', onDragEnter)
-        .on('dragleave', onDragLeave)
-        .on('drop', onDrop);    
-    */
+    var jdataReagent = $.parseJSON($("#hidFavoriteTreeReagent").val());
+
+    $('#favoriteTreeReagent').jstree({
+        "core":{
+            "data":jdataReagent,
+            "check_callback":function(operation,node,node_parent,node_position,more){
+                if (operation=="move_node"){
+                    if(node_parent.icon != "jstree-folder" && node_parent.id != "#"){
+                        return false;
+                    }
+                }
+            }
+        },
+        "plugins":["contextmenu", "dnd"],
+        "contextmenu":{
+            "items": function($node) {
+                var tree = $('#favoriteTreeReagent').jstree(true);
+                return {
+                    "Delete": {
+                        "separator_before": false,
+                        "separator_after": false,
+                        "label": "削除",
+                        "icon": "contextmenu_deleteicon",
+                        "action": function (obj) {
+                            if (confirm('削除しますか？')){
+                                deletekey = $node.original.key;
+                                $.ajax({
+                                    headers: {
+                                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                                    },
+                                    url: 'SearchPage/' + deletekey + '',
+                                    type: 'GET',
+                                    datatype: 'json',
+                                    data : {'update_id' : -1, 'parent_id' : -1, 'delete_id' : deletekey, 'order_number' : -1}
+                                })
+                                // Ajaxリクエスト成功時の処理
+                                .done(function(data) {
+                                    if (data['status'] !== 'OK') {
+                                        alert('データ削除に失敗しました');
+                                    }
+                                    else{
+                                        tree.delete_node($node);
+                                    }
+                                })
+                                // Ajaxリクエスト失敗時の処理
+                                .fail(function(data) {
+                                    alert('データ削除に失敗しました');
+                                });
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }).on('move_node.jstree', function(e, data){
+        /*移動したデータのFavoriteテーブルid*/
+        movenodeKey = data.node.original.key;
+        parentKey = '-1';
+        if (data.parent !== '#') {
+            parentKey = data.instance.get_node(data.node.parent).original.key;        
+        }
+        $.ajax({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            url: 'SearchPage/' + movenodeKey + '',
+            type: 'GET',
+            datatype: 'json',
+            data : {'update_id' : movenodeKey, 'parent_id' : parentKey, 'delete_id' : -1, 'order_number' : -1}
+        })
+        // Ajaxリクエスト成功時の処理
+        .done(function(data) {
+            if (data['status'] !== 'OK') {
+                alert('データ更新に失敗しました');
+            }
+        })
+        // Ajaxリクエスト失敗時の処理
+        .fail(function(data) {
+            alert('データ更新に失敗しました');
+        });
+    });
 
 
+
+    var jdataArticle = $.parseJSON($("#hidFavoriteTreeArticle").val());
+
+    $('#favoriteTreeArticle').jstree({
+        "core":{
+            "data":jdataArticle,
+            "check_callback":function(operation,node,node_parent,node_position,more){
+                if (operation=="move_node"){
+                    if(node_parent.icon != "jstree-folder" && node_parent.id != "#"){
+                        return false;
+                    }
+                }
+            }
+        },
+        "plugins":["contextmenu", "dnd"],
+        "contextmenu":{
+            "items": function($node) {
+                var tree = $('#favoriteTreeArticle').jstree(true);
+                return {
+                    "Delete": {
+                        "separator_before": false,
+                        "separator_after": false,
+                        "label": "削除",
+                        "icon": "contextmenu_deleteicon",
+                        "action": function (obj) {
+                            if (confirm('削除しますか？')){
+                                deletekey = $node.original.key;
+                                $.ajax({
+                                    headers: {
+                                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                                    },
+                                    url: 'SearchPage/' + deletekey + '',
+                                    type: 'GET',
+                                    datatype: 'json',
+                                    data : {'update_id' : -1, 'parent_id' : -1, 'delete_id' : deletekey, 'order_number' : -1}
+                                })
+                                // Ajaxリクエスト成功時の処理
+                                .done(function(data) {
+                                    if (data['status'] !== 'OK') {
+                                        alert('データ削除に失敗しました');
+                                    }
+                                    else{
+                                        tree.delete_node($node);
+                                    }
+                                })
+                                // Ajaxリクエスト失敗時の処理
+                                .fail(function(data) {
+                                    alert('データ削除に失敗しました');
+                                });
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }).on('move_node.jstree', function(e, data){
+        /*移動したデータのFavoriteテーブルid*/
+        movenodeKey = data.node.original.key;
+        parentKey = '-1';
+        if (data.parent !== '#') {
+            parentKey = data.instance.get_node(data.node.parent).original.key;        
+        }
+        $.ajax({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            url: 'SearchPage/' + movenodeKey + '',
+            type: 'GET',
+            datatype: 'json',
+            data : {'update_id' : movenodeKey, 'parent_id' : parentKey, 'delete_id' : -1, 'order_number' : -1}
+        })
+        // Ajaxリクエスト成功時の処理
+        .done(function(data) {
+            if (data['status'] !== 'OK') {
+                alert('データ更新に失敗しました');
+            }
+        })
+        // Ajaxリクエスト失敗時の処理
+        .fail(function(data) {
+            alert('データ更新に失敗しました');
+        });
+    });
 
 
     $("input[name=tabCart]").val([$("input[name=searchFormTab]:checked").val()]);
@@ -42,6 +226,19 @@ jQuery (function ()
             $(".table-searchFixed tbody td:nth-child(3)").css('width',tblwidth + 'px');
         }
     }
+    /*一覧の行をダブルクリック*/
+    $(".table-searchFixed-tr").dblclick(function() {
+        $("#detailAmount").html($(this).children("td").eq(1).html());
+        $("#detailProductName").html($(this).children("td").eq(2).html());
+        $("#detailStandard").html($(this).children("td").eq(3).html());
+        $("#detailCatalogCode").html($(this).children("td").eq(4).html());
+        $("#detailMakerName").html($(this).children("td").eq(5).html());
+        $("#detailUnitPrice").html($(this).children("td").eq(6).html());
+        $("#detailSupplierName").html($(this).children("td").eq(8).html());
+
+        $("#modal-detail").modal('show');
+    });
+
     /*絞込検索　表示・非表示ボタン*/
     $("#toggle-button-searchpage").click(function() {
         $(".leftside-fixed-240").animate( { width: 'toggle', opacity: "toggle"},
@@ -58,6 +255,7 @@ jQuery (function ()
         $('input[name="searchCatalogCodeR"]').val("");
         $('input[name="makerCheckboxR[]"]').prop('checked',false);
     });
+
     /*試薬検索条件エリア　メーカー全て解除/選択*/
     $("#makerCheckboxRAll").click(function () {
         if($(this).is(':checked')) {
@@ -66,6 +264,7 @@ jQuery (function ()
             $('input[name="makerCheckboxR[]"]').prop('checked',false);
         }
     });
+
     /*物品検索条件エリア　クリアボタン*/
     $("#btnClearArticle").click(function() {
         $('input[name="searchReagentNameA"]').val("");
@@ -73,6 +272,7 @@ jQuery (function ()
         $('input[name="searchCasNoR"]').val("");
         $('input[name="makerCheckboxA[]"]').prop('checked',false);
     });
+
     /*試薬検索条件エリア　メーカー全て解除/選択*/
     $("#makerCheckboxAAll").click(function () {
         if($(this).is(':checked')) {
@@ -86,6 +286,7 @@ jQuery (function ()
     $("#submitReagent").click(function() {
         $("#submit_key").val("1");
     });
+
     /*物品　検索ボタンクリック */
     $("#submitArticle").click(function() {
         $("#submit_key").val("2");
@@ -96,6 +297,7 @@ jQuery (function ()
         $("input[name=cartFavorite_submit_key]").val('btnCart');
         $("#submit_key").val("btnCart");
     });
+
     /*お気に入り追加ボタンクリック*/
     $("input[name=btnFavorite]").click(function() {
         $("input[name=cartFavorite_submit_key]").val('btnFavorite');
@@ -106,6 +308,7 @@ jQuery (function ()
     $("#btnFolderAdd").click(function() {
         $("#modal-folderadd").modal('show');
     });
+
     /*フォルダクリアボタン*/
     $("#btnFolderClear").click(function() {
         $("#FolderName").val("");
@@ -115,20 +318,27 @@ jQuery (function ()
     $("input[name=searchFormTab]").change(function() {
         $("input[name=tabCart]").val([$(this).val()]);
         $("input[name=tabFavorite]").val([$(this).val()]);
+        $("input[name=tabSelectFolder]").val($(this).val());
     });
     $("input[name=tabCart]").change(function() {
         $("input[name=searchFormTab]").val([$(this).val()]);
         $("input[name=tabFavorite]").val([$(this).val()]);
+        $("input[name=tabSelectFolder]").val($(this).val());
     });
     $("input[name=tabFavorite]").change(function() {
         $("input[name=tabCart]").val([$(this).val()]);
         $("input[name=searchFormTab]").val([$(this).val()]);
+        $("input[name=tabSelectFolder]").val($(this).val());
     });
     /*選択したarticleに色を付ける*/
     $(".sectionOrderRequest article").click(function() {
         $(".sectionOrderRequest article").removeClass("table-fixed-selectRow");
         $(".sectionOrderRequest article").removeClass("table-fixed-nonselect");
         $(this).addClass("table-fixed-selectRow");
+    });
+
+    $("#chkShared").click(function() {
+        $(".searchConditionForm").submit();
     });
 
 })
