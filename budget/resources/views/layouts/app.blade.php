b08baafc605a80d6174e7c509390ef4c2b3c759b
yoshida.aiko <yoshida.aiko@infogram.co.jp>
2020-09-25 12:09:42 +0900

    ファイル最新化

--- budget/resources/views/layouts/app.blade.php
+++ budget/resources/views/layouts/app.blade.php
@@ -20,9 +20,23 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/styleNakamichi.css') }}" rel="stylesheet">
     <link href="{{ asset('css/navi.css') }}" rel="stylesheet">
-
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    
+    <!-- jstree -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jstree/3.3.2/jstree.min.js" defer></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jstree/3.3.2/themes/default/style.min.css">
+    
+    <!-- jQuery UI -->
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous" defer></script>
+
+    <!--BlockUI-->
+    <script type="text/javascript" src="{{ asset('js/jquery.blockUI.js') }}" defer></script>
+
+    <!--contextmenu-->
+    <script type="text/javascript" src="{{ asset('js/popmenu.js') }}" defer></script>
+    <link href="{{ asset('css/popmenu.css') }}" rel="stylesheet">
 
 </head>
 <body>
@@ -31,7 +45,7 @@
             <div class="container">
                 <div style="width:100%;">
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Budget Management System') }}
+                        {{ config('app.name', '予算管理システム') }}
                     </a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                         <span class="navbar-toggler-icon"></span>
@@ -42,23 +56,23 @@
                         <ul id="myNavbar" class="navbar-nav mr-auto">
                             <li class="current"><a href="{{ route('home') }}" class="nav-menu-home">トップ</a></li>
                             <li><a href="{{ route('SearchPage.index') }}" class="nav-menu-search">検索</a></li>
-                            <li><a href="#" class="nav-menu-orderrequest">発注依頼</a></li>
-                            @if (Auth::user()->UserAuth >= 1)
-                            <li><a href="#" class="nav-menu-order">発注</a></li>
+                            <li><a href="{{ route('OrderRequest.index') }}" class="nav-menu-orderrequest">発注依頼</a></li>
+                            @if (strpos(Auth::user()->UserAuthString,'Order') !== false)
+                            <li><a href="{{ route('Order.index') }}" class="nav-menu-order">発注</a></li>
                             @endif
-                            @if (Auth::user()->UserAuth >= 2)
+                            @if (strpos(Auth::user()->UserAuthString,'Delivery') !== false)
                             <li><a href class="nav-menu-delivery">納品</a></li>
                             @endif
-                            @if (Auth::user()->UserAuth >= 32)
+                            @if (strpos(Auth::user()->UserAuthString,'Payment') !== false)
                             <li><a href="#" class="nav-menu-payment">支払</a></li>
                             @endif
-                            @if (Auth::user()->UserAuth >= 4)
+                            @if (strpos(Auth::user()->UserAuthString,'Budget') !== false)
                             <li><a href="#" class="nav-menu-budget">予算状況</a></li>
                             @endif
-                            @if (Auth::user()->UserAuth >= 8)
+                            @if (strpos(Auth::user()->UserAuthString,'Purchase') !== false)
                             <li><a href="#" class="nav-menu-purchase">購入履歴</a></li>
                             @endif
-                            @if (Auth::user()->UserAuth >= 16) 
+                            @if (strpos(Auth::user()->UserAuthString,'Master') !== false) 
                             <li class="dropdown">
                                 <a class="dropdown-toggle nav-menu-master" href="#" data-toggle="dropdown" >マスタ</a>
                                 <div class="dropdown-menu dropdown-menu-originalcolor" aria-labelledby="navbarDropdown">
@@ -74,6 +88,20 @@
 
                         <!-- Right Side Of Navbar -->
                         <ul class="navbar-login">
+                            <li class="nav-item dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                {{ Config::get('languages')[App::getLocale()] }}
+                            <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                @foreach (Config::get('languages') as $lang => $language)
+                                    @if ($lang != App::getLocale())
+                                        <li>
+                                            <a href="{{ route('lang.switch', $lang) }}">{{$language}}</a>
+                                        </li>
+                                    @endif
+                                @endforeach
+                            </ul>
+                            </li>
                             <!-- Authentication Links -->
                             @guest
                                 <!--<li class="nav-item">
