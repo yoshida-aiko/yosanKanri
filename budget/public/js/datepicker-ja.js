5c53e287cdf2bccb0ad025cd5700afa8252c0f88
yoshida.aiko <yoshida.aiko@infogram.co.jp>
2020-09-25 12:43:14 +0900

    JS　データピッカー配置　スタイルシート　JQueryUI　配置

---
+++ budget/public/js/datepicker-ja.js
@@ -0,0 +1,48 @@
+/* Japanese initialisation for the jQuery UI date picker plugin. */
+/* Written by Kentaro SATO (kentaro@ranvis.com). */
+( function( factory ) {
+	if ( typeof define === "function" && define.amd ) {
+
+		// AMD. Register as an anonymous module.
+		define( [ "../widgets/datepicker" ], factory );
+	} else {
+
+		// Browser globals
+		factory( jQuery.datepicker );
+	}
+
+	$.datepicker._gotoToday = function(id) {
+		var target = $(id);
+		var inst = this._getInst(target[0]);
+		var date = new Date();
+		this._setDate(inst,date);
+		this._hideDatepicker();
+	}
+
+}( function( datepicker ) {
+
+datepicker.regional.ja = {
+	closeText: "閉じる",
+	prevText: "&#x3C;前",
+	nextText: "次&#x3E;",
+	currentText: "今日",
+	monthNames: [ "1月","2月","3月","4月","5月","6月",
+	"7月","8月","9月","10月","11月","12月" ],
+	monthNamesShort: [ "1月","2月","3月","4月","5月","6月",
+	"7月","8月","9月","10月","11月","12月" ],
+	dayNames: [ "日曜日","月曜日","火曜日","水曜日","木曜日","金曜日","土曜日" ],
+	dayNamesShort: [ "日","月","火","水","木","金","土" ],
+	dayNamesMin: [ "日","月","火","水","木","金","土" ],
+	weekHeader: "週",
+	dateFormat: "yy/mm/dd",
+	firstDay: 0,
+	isRTL: false,
+	showMonthAfterYear: true,
+	yearSuffix: "年",
+	showButtonPanel: true // "今日"ボタン, "閉じる"ボタンを表示する
+};
+datepicker.setDefaults( datepicker.regional.ja );
+
+return datepicker.regional.ja;
+
+} ) );
