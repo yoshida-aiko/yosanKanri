330e2afb28cf469d4e8a6b585da306a65fe24492
yoshida.aiko <yoshida.aiko@infogram.co.jp>
2020-10-19 10:57:10 +0900

    langファイル最新化

---
+++ budget/resources/lang/ja/screenwords.php
@@ -0,0 +1,58 @@
+<?php
+
+return [
+
+    /*
+    |-----------------------------------------------------------------
+    | resources/views/layouts/app.blade.php
+    |-----------------------------------------------------------------
+    */
+    'top' => 'トップ',
+    'search' => '検索',
+    'orderRequest' => '発注依頼',
+    'order' => '発注',
+    'delivery' => '納品',
+    'payment' => '支払',
+    'budgetStatus' => '予算状況',
+    'buyingHistory' => '購入履歴',
+    'master' => 'マスタ',
+    'master_user' => 'ユーザーマスタ',
+    'master_supplier' => '発注先マスタ',
+    'master_maker' => 'メーカーマスタ',
+    'master_budget' => '予算マスタ',
+    'master_setting' => '設定',
+    'bulletinBoard' => '掲示板',
+    'newRegister' => '新規登録',
+    'progressReport' => '進捗状況',
+    'refineItem' => '絞込検索',
+    'searchResult' => '検索結果',
+    'orderRequestList' => '発注依頼リスト',
+    'favorite' => 'お気に入り',
+    'reagent' => '試薬',
+    'article' => '物品',
+    'reagentName' => '試薬名',
+    'standard' => '規格',
+    'casNo' => 'CAS No',
+    'catalogCode' => 'カタログコード',
+    'maker' => 'メーカー',
+    'all' => '全て',
+    'type' => '種類',
+    'shared' => '共用',
+    'registerDate' => '登録日',
+    'title' => 'タイトル',
+    'content' => '内容',
+    'displayLimit' => '表示期限',
+    'save' => '保存',
+    'delete' => '削除',
+    'clear' => 'クリア',
+    'close' => '閉じる',
+    'capacity' => '容量',
+    'itemName' => '商品名',
+    'makerName' => 'メーカー名',
+    'unitPrice' => '単価',
+    'noData' => 'データがありません',
+    'itemDetails' => '商品詳細',
+    'prioritySupplier' => '優先する発注先',
+    
+
+];
