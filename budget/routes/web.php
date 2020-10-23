ee74acaf0fd02ff2bd7e86462e3165adcbd79855
yoshida.aiko <yoshida.aiko@infogram.co.jp>
2020-10-23 14:07:38 +0900

    設定画面　POST送信追加

--- budget/routes/web.php
+++ budget/routes/web.php
@@ -62,7 +62,8 @@ Route::group(['middleware' => 'auth'], function() {
     Route::resource('/Supplier','SupplierController');
     Route::resource('/Maker','MakerController');
     Route::resource('/Budget','BudgetController');
-    Route::resource('/Condition', 'ConditionController'); 
+    Route::resource('/Condition', 'ConditionController');
+    Route::post('/Condition', 'ConditionController@judge');
     
     Route::get('pdf','PDFController@index');
     Route::get('lang/{lang}', ['as'=>'lang.switch', 'uses'=>'LanguageController@switchLang']);
