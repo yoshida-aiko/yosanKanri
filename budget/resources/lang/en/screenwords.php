e258537111755251ba7c58fdc588ae50246e27dd
yoshida.aiko <yoshida.aiko@infogram.co.jp>
2020-10-26 15:58:50 +0900

    Lang/en　ファイル最新化

---
+++ budget/resources/lang/en/screenwords.php
@@ -0,0 +1,80 @@
+<?php
+
+return [
+
+    /*
+    |-----------------------------------------------------------------
+    | resources/views/layouts/app.blade.php
+    |-----------------------------------------------------------------
+    */
+    'all' => 'All',
+    'article' => 'Article',
+    
+    'budgetStatus' => 'Budget status',
+    'bulletinBoard' => 'Bulletin Board',
+    'buyingHistory' => 'Buying History',
+    
+    'casNo' => 'CAS No',
+    'catalogCode' => 'Catalog Code',
+    'clear' => 'Clear',
+    'close' => 'Close',
+    'capacity' => 'Capacity',
+    'content' => 'Content',
+    
+    'delete' => 'Delete',
+    'delivery' => 'Delivery',
+    'displayLimit' => 'Display limit',
+    
+    'favorite' => 'Favorite',
+    
+    'itemDetails' => 'Item details',
+    'itemName' => 'Item name',
+    
+    'maker' => 'Maker',
+    'makerName' => 'Maker',
+    'master' => 'Master',
+    'master_user' => 'User Master Maintenance',
+    'master_supplier' => 'Supplier Master Maintenance',
+    'master_maker' => 'Maker Master Maintenance',
+    'master_budget' => 'Budget Master Maintenance',
+    'master_setting' => 'My Setting',
+    
+    'newRegister' => 'New Register',
+    'noData' => 'There is no data.',
+    
+    'orderRequest' => 'Order Request',
+    'order' => 'Order',
+    'orderRequestList' => 'Order Request List',
+    
+    'payment' => 'Payment',
+    'progressReport' => 'Progress report',
+    'prioritySupplier' => 'Priority supplier', 
+    
+    'quantity' => 'Quantity', 
+
+    'reagent' => 'Reagent',
+    'reagentName' => 'Reagent name',
+    'remark' => 'Remark',
+    'requesting' => 'Requesting',
+    'refineItem' => 'Refine Item',
+    
+    'search' => 'Search',
+    'searchResult' => 'Search Result',
+    'standard' => 'Standard',
+    'shared' => 'Shared',
+    'save' => 'Save',
+    'searchHeaderPlaceholder' => 'Search by part of the product name or catalog code',
+
+    'top' => 'Top',
+    'type' => 'Type',
+    
+    'registerDate' => 'Register Date',
+    'requestOrderDate' => 'Request/Order Date',
+    'requester' => 'Requester',
+
+    'title' => 'Title',
+    
+    'unitPrice' => 'Unit Price',
+    
+    'waitingForDelivery' => 'waiting for delivery',   
+];
